d6df8d908f4b57afce4f68d31c24da38a575a70a
User <user@example.com>
2026-09-09 13:22:19 +0300

    chore: remove unused repository abstractions

--- travel_journal_app/lib/repositories/journal_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../models/journal.dart';
-
-abstract class JournalRepository {
-  Future<List<Journal>> getAllJournals();
-  Future<Journal?> getJournalById(int id);
-  Future<void> saveJournal(Journal journal);
-  Future<void> deleteJournal(int id);
-}
